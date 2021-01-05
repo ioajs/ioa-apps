a32c9b5ce312c99c7f843f415227a116278506ef
lixiang <lixiangio@qq.com>
2021-01-05 14:39:55 +0800

    v10.0.5

--- user/index.js
+++ user/index.js
@@ -6,3 +6,12 @@ app.use('@ioa/config');
 app.use('@ioa/koa');
 app.use('@ioa/ormv');
 app.use('@ioa/auth');
+
+app.loader({
+  "test": {
+     level: 30,
+     action() {
+        return 666;
+     }
+  },
+});
