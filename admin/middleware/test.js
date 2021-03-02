f4435f71b693a693d23a8c6fe74d03c60f0bd9f0
lixiang <lixiangio@qq.com>
2021-03-02 12:36:01 +0800

    10.0.6

--- admin/middleware/test.js
+++ admin/middleware/test.js
@@ -1,5 +1,5 @@
 'use strict';
 
 module.exports = async (ctx, next) => {
-   await next()
+   await next();
 }
--- admin/router.js
+++ admin/router.js
@@ -4,12 +4,12 @@ const { router, middleware } = require('@app');
 
 const { test } = middleware;
 
-router.get('/admin', test, 'home.index')
+router.get('/admin', test, 'home.index');
 
-router.get('/admin/:id', 'home.details')
+router.get('/admin/:id', 'home.details');
 
-router.post('/admin', 'home.add')
+router.post('/admin', 'home.add');
 
-router.put('/admin/:id', 'home.update')
+router.put('/admin/:id', 'home.update');
 
-router.delete('/admin/:id', 'home.delete')
+router.delete('/admin/:id', 'home.delete');
